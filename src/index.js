96e901bc16051593ed3d0a66da778c1415494341
Shobhit Jaryal <jaryalshaurya2203@gmial.com>
2022-08-13 16:56:32 +0530

    routes added

--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ import { createRoot } from "react-dom/client";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import rootReducer from "./reducers/rootReducer";
+import { BrowserRouter } from "react-router-dom";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
   <Provider store={createStore(rootReducer)}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>
 );
